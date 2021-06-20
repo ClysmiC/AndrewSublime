540abd3f68ef4274df262e485c507f6eda6d3c9b
Andrew <andrew@alsmith.net>
2021-06-20 11:53:08 -0700

    .

--- als_emacs.py
+++ als_emacs.py
@@ -1,4 +1,6 @@
 # TODO
+# - Context sensitive status bar (e.g. match # 32/77)
+# - change name from i-search to r-search when reverse-searching? (or i-search (r) ?)
 # - Indent instead of inserting tab when I press tab/shift tab with a single line selected
 # - If initial text matches something but then the text gets changed, start back from the beginning of the search
 # - Make sure searches are highlighted in scroll bar (and maybe minimap?)
@@ -11,6 +13,7 @@
 # - Jump focus to other sublime window (not just other view)
 # - Consolidate all open tabs to 1 window. Option to close duplicates?
 # - Force h on left and cpp on right?
+# - virtual whitespace? maybe remap ctrl-alt-arrow to navigate
 
 import sublime
 import sublime_plugin
@@ -18,17 +21,18 @@ import sublime_plugin
 from enum import Enum
 import traceback #debug
 
-# --- Extra state stored per view and per window
+# --- Extra state stored per view
 
 class ViewEx():
 	dictionary = {}
 
 	def __init__(self, view):
+		self.view = view
 		self.markSel = MarkSel(view)
-		# self.iSearch = ISearch(view, self.markSel)
 
 	@staticmethod
 	def get(view):
+
 		result = ViewEx.dictionary.get(view.id())
 
 		if result is None:
@@ -37,14 +41,39 @@ class ViewEx():
 
 		return result
 
-	@staticmethod
-	def onClose(view):
-		ViewEx.dictionary.pop(view.id())
+	def onClose(self):
+		# TODO - Hook this callback up to either on_close(..) or on_pre_close(..)
+		ViewEx.dictionary.pop(self.view.id())
+
+	def entireViewRegion(self):
+		result = sublime.Region(0, self.view.size())
+		return result
+
+# LOG_DEBUG = "DEBUG"
+# LOG_INIT = "init2"
+# LOG_HIDE_PANEL_THEN_RUN = "als_hide_panel_then_run"
+# LOG_EVENTS = "event listener"
+# LOG_ISEARCH = "i-search"
+
+LOG_DEBUG = None
+LOG_INIT = None
+LOG_HIDE_PANEL_THEN_RUN = None
+LOG_EVENTS = None
+LOG_ISEARCH = None
+
+
+
+
+
+
+# --- Extra state stored per view window
 
 class WindowEx():
 	dictionary = {}
 
 	def __init__(self, window):
+		alsTrace(LOG_INIT, "Ctor for window with id " + str(window.id()))
+		self.window = window
 		self.iSearch = ISearch(window)
 
 	@staticmethod
@@ -57,9 +86,18 @@ class WindowEx():
 
 		return result
 
-	@staticmethod
-	def onClose(window):
-		WindowEx.dictionary.pop(window.id())
+	def onClose(self):
+		# TODO - Hook this callback up to either on_close(..) or on_pre_close(..)
+		WindowEx.dictionary.pop(self.window.id())
+
+	def showCustomStatus(self, text):
+		# HMM - Should we set it for all views? Weird that this is something we set per-view, despite being displayed per-window
+		#	What about new views that get created afterwards...?
+		self.window.active_view().set_status("als_custom", text)
+
+	def clearCustomStatus(self):
+		# HMM - Should we set it for all views? Weird that this is something we set per-view, despite being displayed per-window
+		self.window.active_view().erase_status("als_custom")
 
 
 # --- Mark/selection (similar to 'transient-mark-mode' in emacs)
@@ -243,13 +281,32 @@ class AlsInflateSelectionToFillLines(sublime_plugin.TextCommand):
 		markSel = MarkSel.get(self.view)
 
 		oldRegion = markSel.primaryRegion()
-		# newRegion = MarkSel.extendRegion(oldRegion, self.view.line(oldRegion))
+		newRegion = MarkSel.extendRegion(oldRegion, self.view.line(oldRegion))
 		markSel.select(newRegion, MarkAction.SET)
+
+class AlsOtherView(sublime_plugin.WindowCommand):
+	def run(self):
+		if self.window.num_groups() != 2:
+			# HMM - This API is pretty shit, but I think it's the only thing we can do to force 2 groups?
+			self.window.set_layout({
+			    "cols": [0, 0.5, 1],
+			    "rows": [0, 1],
+			    "cells": [[0, 0, 1, 1], [1, 0, 2, 1]]
+			})
+			self.window.focus_group(1)
+		else:
+			iGroupActive = self.window.active_group()
+			iGroupActiveDesired = (iGroupActive + 1) % self.window.num_groups()
+			self.window.focus_group(iGroupActiveDesired)
 
 class AlsHidePanelThenRun(sublime_plugin.WindowCommand):
 	"""Auto-close a panel and jump right back into the normal view with a command"""
 	def run(self, **kwargs):
+
 		iSearch = ISearch.get(self.window)
+		command_name = kwargs.pop('command_name')
+
+		alsTrace(LOG_HIDE_PANEL_THEN_RUN, str(kwargs))
 
 		self.window.run_command("hide_panel")
 
@@ -261,8 +318,6 @@ class AlsHidePanelThenRun(sublime_plugin.WindowCommand):
 		# NOTE - As of Sublime Text 4.0, calling run_command won't give your hooks a chance to intercept/modify it,
 		#  which breaks the transient mark. So we manually call the hook before dispatching :)
 
-		command_name = kwargs.pop('command_name')
-
 		modified = None
 		if AlsEventListener.instance:
 			# HMM - It might be possible for this to run before AlsEventListener.__init__(..), but it seems very unlikely?
@@ -272,23 +327,42 @@ class AlsHidePanelThenRun(sublime_plugin.WindowCommand):
 
 
 		if modified is None:
-			print(f"running {command_name} unmodified")
+			alsTrace(f"running {command_name} unmodified")
 			view.run_command(command_name, kwargs)
 		else:
 			lastModified = (command_name, kwargs)
 			while modified is not None:
-				print(f"modified {lastModified[0]} into {modified[0]}")
+				alsTrace(LOG_HIDE_PANEL_THEN_RUN, f"modified {lastModified[0]} into {modified[0]}")
 				lastModified = modified
 				modified = AlsEventListener.instance.on_text_command(view, modified[0], modified[1])
 
-			print(f"running {lastModified[0]} (modified)")
+			alsTrace(LOG_HIDE_PANEL_THEN_RUN, f"running {lastModified[0]} (modified)")
 			view.run_command(lastModified[0], lastModified[1])
 
 # --- I-Search
+#		https://www.gnu.org/software/emacs/manual/html_node/emacs/Repeat-Isearch.html
+#		Features implemented from spec:
+#			X search forward
+#			x search backward
+#			x highlight other matches
+#			x repeat previous search
+#			X case sensitivity determined by input		#	(default setting buried elsewhere in spec: https://www.gnu.org/software/emacs/manual/html_node/emacs/Special-Isearch.html)
+#			* wrapped									#	Dropped. I prefer to auto-wrap without an extra search command.
+#			_ overwrapped								#	HMM - Maybe show in status bar?
+#			*  search ring								#	Dropped. I don't think I'd use this.
 
-# TODO
-# ‘Overwrapped’ search, which means that you are revisiting matches that you have already seen (i.e., starting from cursorOnOpen)
+#		Custom features:
+#			_ Show previous focus as a different tint once your search starts failing. Could we even show a red tint in the inputView by adding a region to it? That'd be fuckin metal.
+#			_ Show outlines for matches in inactive views too? Have an easy hotkey to jump to next window?
 
+#		Known bugs:
+#		!!!	_ ctrl+s, esc, ctrl+s crashes sublime. (maybe any time you do a second i-search?)
+#				commenting out onDeactivated fixes the issue. But I want that functionality :(
+#			_ region only expands. if you search forward a couple times then going backwards should cause it to retract.
+
+#		Fixed bugs
+#			2021
+#				6/15: move_to now properly exits the input panel when running i-search
 
 class ISearch():
 
@@ -299,7 +373,6 @@ class ISearch():
 	FOUND_REGION_NAME = "als_find_highlight"
 	FOCUS_REGION_NAME = "als_find_focus"
 	EXTRA_SELECTION_REGION_NAME = "als_find_extra_selection"
-	DEBUG_LOG = False
 
 	class Focus:
 
@@ -329,37 +402,70 @@ class ISearch():
 		self.focus = ISearch.NO_FOCUS
 		self.forward = True
 
+		alsTrace(LOG_ISEARCH, f"cleanup({isAfterClose})")
 		if isAfterClose:
+			# WindowEx.get(self.window).clearCustomStatus()
+
+			alsTrace(LOG_ISEARCH, f"cleanup - getmarksel")
 			activeView = self.window.active_view()
 			markSel = MarkSel.get(activeView)
+
+			alsTrace(LOG_ISEARCH, f"cleanup - marksel show selection")
 			markSel.showSelection()
+
+			alsTrace(LOG_ISEARCH, f"cleanup - cleanup drawings")
 			self.cleanupDrawings(activeView)
+
+			alsTrace(LOG_ISEARCH, f"cleanup - clear mark ?")
 			if not markSel.isMarkActive():
+				alsTrace(LOG_ISEARCH, f"yes")
 				markSel.clearAll()
+			else:
+				alsTrace(LOG_ISEARCH, f"no")
+
+			alsTrace(LOG_ISEARCH, f"all done")
 
 	# --- Visibility
 
 	def isShowing(self):
-		return bool(self.inputView and self.inputView.window())
+		return self.inputView and self.inputView.window()
 
 	def open(self, forward=True):
+
 		markSel = MarkSel.get(self.window.active_view())
 		self.cursorOnOpen = markSel.primaryCursor()
 		self.forward = forward
 		self.focus = ISearch.NO_FOCUS
 
 		if not self.isShowing():
-			self.inputView = self.window.show_input_panel(ISearch.PANEL_NAME, self.lastSavedSearch, self.onDone, self.onChange, self.onCancel)
+			# NOTE - Not using self.lastSavedSearch as initial text. That requires a manual re-trigger of search(..) with an empty search string
+			self.inputView = self.window.show_input_panel(ISearch.PANEL_NAME, "", self.onDone, self.onChange, self.onCancel)
+			alsTrace(LOG_ISEARCH, f"open input view ({self.inputView.id()}) : {self.inputView.element()}")
+			alsTrace(LOG_ISEARCH, f"Type: {type(self.inputView)}")
+			alsTrace(LOG_ISEARCH, f"is input view truthy?")
+			alsTrace(LOG_ISEARCH, f"{bool(self.inputView)}")
+			alsTrace(LOG_ISEARCH, f"is input view None?")
+			alsTrace(LOG_ISEARCH, f"{bool(self.inputView is None)}")
+			self.inputViewEx = ViewEx.get(self.inputView)
+			alsTrace(LOG_ISEARCH, f"inputViewEx is ({self.inputViewEx})")
+			self.inputMarkSel = self.inputViewEx.markSel
+			alsTrace(LOG_ISEARCH, f"inputMarkSel is ({self.inputMarkSel})")
 
-		if not self.inputView:		raise AssertionError("i-search has no input view?")
+		alsTrace(LOG_ISEARCH, f"should I assert?")
+		if not self.inputView:
+			alsTrace(LOG_ISEARCH, f"y")
+			raise AssertionError("i-search has no input view?")
+		else:
+			alsTrace(LOG_ISEARCH, f"no")
 
+		alsTrace(LOG_ISEARCH, f"stealing focus")
 		self.window.focus_view(self.inputView)
-		self.inputView.run_command("select_all")
+		alsTrace(LOG_ISEARCH, f"done stealing focus")
+		# self.inputView.run_command("select_all")	# HMM/TODO - This seems to somehow clear the text?.... which is actually what I want...
 
 	def close(self):
-		if self.isShowingInputView():
+		if self.isShowing():
 			self.window.run_command("hide_panel")
-			self.cleanup(isAfterClose=True)
 
 	def cleanupDrawings(self, view):
 		view.erase_regions(ISearch.FOUND_REGION_NAME)
@@ -369,32 +475,63 @@ class ISearch():
 	# --- Hooks
 
 	def onTextCommand(self, command_name, args):
+		alsTrace(LOG_ISEARCH, f"onTextCommand: {command_name} | {str(args)}")
 		# HMM - I'd rather use (poorly documented) "chain" command here, but that doesn't let me hook into the chained commands :(
+		#		Window seems to and Views seem not to but I've read on the internet that window sometimes messes up too... do I really
+		#		need to write my own custom dispatcher?
 
 		if command_name == "move" or command_name == "move_to":
 			args["command_name"] = command_name
-			self.inputView.run_command("als_hide_panel_then_run", args)
+			self.window.run_command("als_hide_panel_then_run", args)
 			return ("", None)
 
 		return None
 
+	def onDeactivated(self):
+		self.close()
+
 	def onDone(self, text):
-		print("INPUT DONE")
+		alsTrace(LOG_ISEARCH, f"onChange")
 		self.text = text # HMM - probably unnecessary?
 		self.lastSavedSearch = text
 		self.cleanup(isAfterClose=True)
 
 	def onChange(self, text):
+		alsTrace(LOG_ISEARCH, f"onChange")
 		self.text = text
-		self.search(isRepeatedSearch=False)
+		if self.text:
+			self.search(isRepeatedSearch=False)
+		else:
+			pass	# TODO - Any state we want to clean up here, if they type stuff and then delete all the way back?
 
 	def onCancel(self):
+		alsTrace(LOG_ISEARCH, f"onCancel")
 		self.cleanup(isAfterClose=True)
 
 	# --- Operations
 
-	def search(self, isRepeatedSearch):
+	def search(
+		self,
+		isRepeatedSearch,
+		edit=None):		# NOTE - required iff isRepeatedSearch
 
+		alsTrace(LOG_ISEARCH, f"BEGIN SEARCH FOR {self.text}")
+
+		if isRepeatedSearch != (edit is not None):		raise AssertionError("Repeated searches require an 'edit' to behave properly in the case of an empty 'self.text'")
+
+		# @HACK - If re-loading last saved search, we just set the text, and trust that the onChange hook will
+		#	recurse back into search(..). For this reason, the function is re-entrant and returns after changing
+		#	text (which triggers onChange)
+
+		if not self.text:
+			if self.lastSavedSearch and edit:
+				self.inputView.replace(edit, self.inputViewEx.entireViewRegion(), self.lastSavedSearch)
+				self.inputMarkSel.clearAll()
+				self.inputView.run_command("move_to", { "to": "eof", "extend": False })
+			else:
+				return
+
+		# windowEx = WindowEx.get(self.window)
 		markSel = MarkSel.get(self.window.active_view())
 		keepMark = markSel.isMarkActive()
 		markAction = MarkAction.KEEP if keepMark else MarkAction.CLEAR # @Redundant with keepMark
@@ -437,9 +574,11 @@ class ISearch():
 			# Choose best match
 
 			debug_matchPrev = sublime.Region(-1, -1)
-			bestMatch = None
+			iBest = -1
 
-			for match in found:
+			for i in range(len(found)):
+
+				match = found[i]
 
 				if match.b < match.a:
 					raise AssertionError("find_all returned reversed region?")
@@ -449,28 +588,32 @@ class ISearch():
 
 				if self.forward and match.a >= searchFrom:
 					# NOTE - This one breaks the loop, since we can only get further away in an increasing list
-					bestMatch = match
+					iBest = i
 					break
 
 				elif not self.forward and match.b <= searchFrom:
 					# NOTE - This one doesn't, since we can only get closer and continue updating our ideal
-					bestMatch = match
+					iBest = i
 
 				debug_matchPrev = match
 
 			wrappedAround = False
-			if not bestMatch:
-				if self.forward: 	bestMatch = found[0]	# wrap around to top match, HMM - require extra keypress to commit to wraparound?
-				else:				bestMatch = found[-1]	# ... to bot match ...
+			if iBest == -1:
+				if self.forward: 	iBest = 0				# wrap around to top match, HMM - require extra keypress to commit to wraparound?
+				else:				iBest = len(found) -1	# ... to bot match ...
 				wrappedAround = True
 
 			# --- Lock in the match
 
+			bestMatch = found[iBest]
 			self.focus = ISearch.Focus(ISearch.Focus.State.ACTIVE if isRepeatedSearch else ISearch.Focus.State.PASSIVE,
 										bestMatch)
 
 			markSel.select(self.focus.region, markAction, extend=keepMark)
 			markSel.hideSelection()
+
+			
+			# windowEx.showCustomStatus(f"Match {iBest + 1} of {len(found)}")
 
 			primaryRegion = markSel.primaryRegion()
 			extraSelection = MarkSel.subtractRegion(primaryRegion, self.focus.region)
@@ -494,20 +637,20 @@ class ISearch():
 				extraSelection,
 				scope=ISearch.EXTRA_SELECTION_REGION_NAME)
 
-			if ISearch.DEBUG_LOG:
-				if wrappedAround:
-					if self.forward:	print(f"wraparound match found at ({match.a}, {match.b}) - ideal start: {searchFrom})")
-					else:				print(f"wraparound match (r) found at ({match.a}, {match.b}) - ideal end: {searchFrom})")
-				else:
-					if self.forward:	print(f"match found at ({match.a}, {match.b}) - ideal start: {searchFrom})")
-					else:				print(f"match (r) found at ({match.a}, {match.b}) - ideal end: {searchFrom})")
+			if wrappedAround:
+				if self.forward:	alsTrace(LOG_ISEARCH, f"wraparound match found at ({match.a}, {match.b}) - ideal start: {searchFrom})")
+				else:				alsTrace(LOG_ISEARCH, f"wraparound match (r) found at ({match.a}, {match.b}) - ideal end: {searchFrom})")
+			else:
+				if self.forward:	alsTrace(LOG_ISEARCH, f"match found at ({match.a}, {match.b}) - ideal start: {searchFrom})")
+				else:				alsTrace(LOG_ISEARCH, f"match (r) found at ({match.a}, {match.b}) - ideal end: {searchFrom})")
 		else:
 			# TODO - play beep here?
+			# windowEx.showCustomStatus(f"No matches")
 			self.focus = ISearch.Focus(ISearch.Focus.State.NIL, None)
 			self.cleanupDrawings(activeView)
-			if ISearch.DEBUG_LOG:
-				if self.forward:	print("No match found")
-				else: 				print("No match (r) found")
+
+			if self.forward:	alsTrace(LOG_ISEARCH, "No match found")
+			else: 				alsTrace(LOG_ISEARCH, "No match (r) found")
 
 
 
@@ -521,7 +664,7 @@ class AlsIncrementalSearch(sublime_plugin.TextCommand):		# NOTE - TextCommand in
 		# --- Detect re-search
 		if self.view.element() == ISearch.INPUT_ELEMENT:
 			iSearch.forward = forward
-			iSearch.search(isRepeatedSearch=True)
+			iSearch.search(isRepeatedSearch=True, edit=edit)
 			return
 
 		# --- Bail if inside some other special view
@@ -535,7 +678,6 @@ class AlsIncrementalSearch(sublime_plugin.TextCommand):		# NOTE - TextCommand in
 # --- Listeners/Hooks
 
 class AlsEventListener(sublime_plugin.EventListener):
-	DEBUG_LOG = False
 
 	instance = None
 
@@ -547,7 +689,7 @@ class AlsEventListener(sublime_plugin.EventListener):
 		# IMPORTANT - Altering the return value feeds the altered command back into this function.
 		#  Only return a tuple if you have actually altered things, otherwise there is inifnite recursion!
 
-		if self.DEBUG_LOG:	print("[event listener] text_command: " + command_name)
+		alsTrace(LOG_EVENTS, "[event listener] text_command: " + command_name)
 
 		# Maybe dispatch to input view
 
@@ -559,7 +701,6 @@ class AlsEventListener(sublime_plugin.EventListener):
 
 		markSel = MarkSel.get(view)
 		if command_name == "move" or command_name == "move_to":
-
 			if markSel.isMarkActive() and not args.get("extend", False):
 				args["extend"] = True
 				return (command_name, args)
@@ -582,7 +723,7 @@ class AlsEventListener(sublime_plugin.EventListener):
 		# NOTE - Commands that don't modify the buffer are handled here.
 		#  All buffer-modifying commands are handled in on_modified
 
-		if self.DEBUG_LOG:	print("[event listener] on_post_text_command")
+		alsTrace(LOG_EVENTS, "[event listener] on_post_text_command")
 
 		if command_name == "copy":
 			markSel = MarkSel.get(view)
@@ -592,7 +733,7 @@ class AlsEventListener(sublime_plugin.EventListener):
 		# NOTE - Anything that affects contents of buffer will hook into
 		#  this function
 
-		if self.DEBUG_LOG:	print("[event listener] on_modified")
+		alsTrace(LOG_EVENTS, "[event listener] on_modified")
 
 		markSel = MarkSel.get(view)
 		if markSel is None:
@@ -604,12 +745,37 @@ class AlsEventListener(sublime_plugin.EventListener):
 			markSel.clearAll()
 
 	def on_window_command(self, window, command_name, args):
-		if self.DEBUG_LOG: print("[event listener] window_command: " + command_name)
+		alsTrace(LOG_EVENTS, "[event listener] window_command: " + command_name)
 
 		return None
 
 	def on_activated(self, view):
-		if self.DEBUG_LOG: print("view activated: " + str(view.element()))
+		alsTrace(LOG_EVENTS, "view activated: " + str(view.element()))
 
-	def on_deactivated(self, view):
-		if self.DEBUG_LOG: print("view deactivated: " + str(view.element()))
+	# FIXME - If uncommented, always crashing the second search!!!!!
+	# def on_deactivated(self, view):
+	# 	alsTrace(LOG_EVENTS, "view deactivated: " + str(view.element()))
+
+	# 	# Maybe dispatch to input view
+
+	# 	if view.element() == ISearch.INPUT_ELEMENT:
+	# 		iSearch = ISearch.get(view.window())
+	# 		return iSearch.onDeactivated()
+
+	def on_exit(self):
+		pass
+
+def plugin_loaded():
+	with open('als_trace.txt','w') as file:
+	    pass	# NOTE - Clears file
+
+# TODO - alsAssert which logs + exits on failure, instead of raising AssertionError
+def alsTrace(tag, line):
+	if tag:
+		LOG_TO_FILE = False
+		if LOG_TO_FILE:
+			# SLOW - opening file every time we log lol
+			with open('als_trace.txt', 'a') as file:
+				file.write(f"[{tag}] {line}\n")
+		else:
+			print(line)
